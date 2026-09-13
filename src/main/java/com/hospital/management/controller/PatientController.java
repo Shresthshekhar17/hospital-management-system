90c1574cf206dd626838bc6ea5f120ded01b8d27
Shresth Shekhar <royshresth8@gmail.com>
2026-09-14 04:25:39 +0530

    Initial project setup

---
+++ src/main/java/com/hospital/management/controller/PatientController.java
@@ -0,0 +1,10 @@
+package com.hospital.management.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/patients")
+public class PatientController {
+
+}
